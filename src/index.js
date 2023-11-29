cbe18c5525b72dfbfa58e580344bb9c2ad80844c
RichardJC Wu <richardjc.wu@moxa.com>
2023-11-29 13:38:13 +0800

    react tutorial 053

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import react library
+import React from "react";
+//import react DOM
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//render app
+// 使用 createRoot 替代 ReactDOM.render
+const root = createRoot(document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 使用 root.render 代替 ReactDOM.render
+root.render(<App />);
